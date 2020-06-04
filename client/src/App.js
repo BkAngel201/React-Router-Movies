320a6f0736402424f0b92bc9f63b5491e9b157ff
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-06-03 22:28:34 -0400

    Project and stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route, Switch } from "react-router-dom";
+
 import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -30,24 +28,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Switch>
-        <Route
-          path="/movies/:movieId"
-          render={props => (
-            <Movie {...props} movies={movieList} addToSavedList={addToSavedList}/>
-          )}
-        >
-
-        </Route>
-        <Route
-          path="/"
-          render={props =>(
-            <MovieList {...props} movies={movieList}/>
-          )}
-        />
-      </Switch>
+      <div>Replace this Div with your Routes</div>
     </div>
-    
   );
 };
 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom'
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <NavLink className="saved-movie" to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+      <span className="saved-movie">{movie.title}</span>
     ))}
-    <Link to='/' className="home-button">Home</Link>
+    <div className="home-button">Home</div>
   </div>
 );
 
